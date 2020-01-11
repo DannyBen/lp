1c1f6c6aa629f419e56ddc3f37e06c6a0a611d96
Danny Ben Shitrit <db@dannyben.com>
2020-01-11 15:22:17 +0000

    resolve ruby warnings

--- lib/lp.rb
+++ lib/lp.rb
@@ -8,16 +8,13 @@ def lp(anything)
   reset   = "\e[0m"
   red     = "\e[31m"
   green   = "\e[32m"
-  yellow  = "\e[33m"
   blue    = "\e[34m"
-  magenta = "\e[35m"
-  cyan    = "\e[36m"
 
   result = anything.to_yaml
   
-  result.gsub! /!ruby\/object:/, "#{blue}>>> #{reset}"
-  result.gsub! /([\s\-]*:?.*?:)(.*)/, green + '\1' + reset + '\2'
-  result.gsub! /([&*]\d+)/, red + '\1' + reset
+  result.gsub!(/!ruby\/object:/, "#{blue}>>> #{reset}")
+  result.gsub!(/([\s\-]*:?.*?:)(.*)/, green + '\1' + reset + '\2')
+  result.gsub!(/([&*]\d+)/, red + '\1' + reset)
 
   puts result
 end
